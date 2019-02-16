ae1ccd02b964ba8d3cbf5714c6b2ac9601f5a519
juzisang <juziexe@gmail.com>
2019-02-16 17:30:10 +0800

    feat(config): update

--- README.md
+++ README.md
@@ -1,22 +1,106 @@
-# vue-tsx
+Vue-Cli3 中虽然提供了 TypeScript 写 Vue，但是由于插件的支持度不够，Vue 文件里写 Ts，体验貌似也没什么提升，所以，我决定使用 TSX 来写 Vue
 
-> 使用 TSX 来编写 Vue
+接下来，我以 Vue 初始项目为例，演示怎么将 Vue 文件转换成 Tsx
 
-## cssModule
+## 新建项目
 
-样式文件，必须以`*.module.*`命名，会在同级目录下，生成 `*.d.ts`
+我们使用 Vue-Cli3 新建一个项目，配置如下
 
-## .sync
+![vue-cli-options](screenshot/vue-cli-options.png)
 
-使用`$sync`代替`.sync`
+## vue-tsx-support 增强提示
 
-## v-model
+```
+yarn add vue-tsx-support -D
+```
 
-和 vue 文件写法相同，并且支持自定义`v-model`
+专门为了 Vue Tsx 文件，而编写的一个依赖，详细请阅读[官网](https://github.com/wonderful-panda/vue-tsx-support)
 
-## 依赖库
+或者参考我的项目里 https://github.com/JuZiSang/blog 的用法
 
-- [vue-jsx-hot-loader](https://github.com/skyrpex/vue-jsx-hot-loader)
-- [jsx-v-model](https://github.com/nickmessing/babel-plugin-jsx-v-model)
-- [vue-jsx-sync](https://github.com/njleonzhang/babel-plugin-vue-jsx-sync)
-- [typings-for-css-modules-loader](https://github.com/Jimdo/typings-for-css-modules-loader)
+## Tsx 热重置
+
+### 所需依赖
+
+```bash
+yarn add vue-jsx-hot-loader -D
+```
+
+### vue.config.js
+
+在 `chainWebpack` 增加如下 loader，支持热重置
+
+```js
+config.module
+  .rule("tsx")
+  .test(/\.tsx$/)
+  .use("vue-jsx-hot-loader")
+  .before("babel-loader")
+  .loader("vue-jsx-hot-loader");
+```
+
+## CSSModule 支持
+
+在 `Tsx` 中，`Vue` 原来的 `scoped` 方案就失效了，所以我决定使用 `css-module` 解决这个问题，并且配合 `typings-for-css-modules-loader` 生成 `*.d.ts` 增加提示
+
+### 依赖
+
+```bash
+yarn add typings-for-css-modules-loader node-sass sass-loader -D
+```
+
+### vue.config.js
+
+```js
+["css", "less", "scss", "sass", "stylus", "postcss"].forEach(rule => {
+  // rules for *.module.* files
+  // 将 css-loader 替换为 typings-for-css-modules-loader
+  const cssRule = config.module
+    .rule(rule)
+    .oneOf("normal-modules")
+    .uses.get("css-loader")
+    .set("loader", "typings-for-css-modules-loader");
+});
+```
+
+我们可以继续在 `vue.config.js` 的 css 选项中配置我们的 css，详情[戳这里](https://cli.vuejs.org/zh/config/#css-modules)
+
+```js
+{
+  loaderOptions: {
+    css: {
+      // name 导出
+      namedExport: true,
+      // 是否使用驼峰
+      camelCase: true,
+      // html 中 实际的 css 类名
+      localIdentName: process.env.NODE_ENV !== "production" ? "[local]-[hash:base64:5]" : "[hash:base64:5]"
+    }
+  }
+};
+```
+
+## 支持 sync v-model
+
+### 依赖
+
+```bash
+yarn add babel-plugin-jsx-v-model babel-plugin-vue-jsx-sync -D
+```
+
+### babel.config.js
+
+```js
+module.exports = {
+  presets: ["@vue/app"],
+  plugins: ["vue-jsx-sync", "jsx-v-model"]
+};
+```
+
+## 总结
+
+```
+yarn add vue-tsx-support vue-jsx-hot-loader typings-for-css-modules-loader node-sass sass-loader babel-plugin-jsx-v-model babel-plugin-vue-jsx-sync -D
+```
+
+经过如上改造，就能愉快的编写 Tsx 了
--- vue.config.js
+++ vue.config.js
@@ -3,101 +3,51 @@ const path = require("path");
 module.exports = {
   productionSourceMap: false,
   chainWebpack(config) {
-    setAlias(config);
+    // config.resolve.alias.set("src", path.resolve(__dirname, "src"));
+    // 热重载
     if (process.env.NODE_ENV !== "production") {
-      hotReload(config);
+      config.module
+        .rule("tsx")
+        .test(/\.tsx$/)
+        .use("vue-jsx-hot-loader")
+        .before("babel-loader")
+        .loader("vue-jsx-hot-loader");
     }
+    // bundle-analyzer
     if (process.env.npm_config_report) {
-      openBundleAnalyzer(config);
+      config.plugin("webpack-bundle-analyzer").use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin);
     }
-    ignoreCssWarnings(config);
-    cssModules(config);
+    // 忽略提示
+    // ignoreCssWarnings(config);
+    // css to typings-for-css-modules-loader
+    ["css", "less", "scss", "sass", "stylus", "postcss"].forEach(rule => {
+      // rules for *.module.* files
+      const cssRule = config.module
+        .rule(rule)
+        .oneOf("normal-modules")
+        .uses.get("css-loader")
+        .set("loader", "typings-for-css-modules-loader");
+    });
+  },
+  css: {
+    loaderOptions: {
+      css: {
+        namedExport: true,
+        camelCase: true,
+        localIdentName: process.env.NODE_ENV !== "production" ? "[local]-[hash:base64:5]" : "[hash:base64:5]"
+      }
+    }
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          "^/api": "/"
+        }
+      }
+    }
   }
 };
-
-/**
- * 忽略css警告
- */
-function ignoreCssWarnings(config) {
-  // Define the class
-  class FilterPlugin {
-    constructor(options) {
-      this.options = options;
-    }
-
-    apply(compiler) {
-      compiler.hooks.afterEmit.tap("FilterPlugin", compilation => {
-        compilation.warnings = compilation.warnings.filter(
-          warning => !this.options.filter.test(warning.message)
-        );
-      });
-    }
-  }
-
-  config
-    .plugin("filter-css-warnings-plugin")
-    .use(
-      new FilterPlugin({
-        filter: /Conflicting order between:/
-      })
-    )
-    .end();
-}
-
-/**
- * 设置别名
- */
-function setAlias(config) {
-  // config.resolve.alias.set("src", path.resolve(__dirname, "src"));
-}
-
-/**
- * 启动webpack-bundle-analyzer
- */
-function openBundleAnalyzer(config) {
-  config
-    .plugin("webpack-bundle-analyzer")
-    .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin);
-}
-
-/**
- * 热重载
- */
-function hotReload(config) {
-  config.module
-    .rule("tsx")
-    .test(/\.tsx$/)
-    .use("vue-jsx-hot-loader")
-    .before("babel-loader")
-    .loader("vue-jsx-hot-loader");
-}
-
-/**
- * 自动生成 css.d.ts
- */
-function cssModules(config) {
-  ["css", "less", "scss", "sass", "stylus", "postcss"].forEach(rule => {
-    ["normal-modules"].forEach(oneOf => {
-      config.module
-        .rule(rule)
-        .oneOf(oneOf)
-        .uses.delete("css-loader");
-      config.module
-        .rule(rule)
-        .oneOf(oneOf)
-        .use("typings-for-css-modules-loader")
-        .before("postcss-loader")
-        .loader("typings-for-css-modules-loader")
-        .options({
-          modules: true,
-          namedExport: true,
-          camelCase: true,
-          importLoaders: 2,
-          localIdentName:
-            process.env.NODE_ENV !== "production"
-              ? "[local]-[hash:base64:5]"
-              : "[hash:base64:5]"
-        });
-    });
-  });
-}
